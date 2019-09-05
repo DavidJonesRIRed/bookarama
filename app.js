1b6ea401232a15c3e19a5dcc4071f371b7adfaa4
David Jones <coreldavid@hotmail.com>
2019-09-05 18:44:02 -0400

    just started

--- app.js
+++ app.js
@@ -1,25 +1,6 @@
-var btns = document.querySelectorAll("#book-list .delete");
+const list =document.querySelector("book-list ul");
 
-console.log(btns);
-
-Array.from(btns).forEach(function(btn)
+list.addEventListener("click", function(e)
 {
-    //console.log(btn);
 
-    btn.addEventListener("click",function(e)
-    {
-        //console.log(e);
-        
-        const li = e.target.parentElement;
-        
-        li.parentNode.removeChild(li);
-    });
-});
-
-const link = document.querySelector("#page-banner a");
-
-link.addEventListener("click",function(e)
-{
-    e.preventDefault();
-    console.log("navigation to",e.target.textContent,"was prevented");
 });
