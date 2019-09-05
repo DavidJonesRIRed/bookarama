4f86e71e8dadd5612743f26715d00249b73eb202
David Jones <coreldavid@hotmail.com>
2019-09-05 14:33:43 -0400

    query selections

--- app.js
+++ app.js
@@ -1,3 +1,14 @@
-const wrap = document.querySelector("#wrapper");
+//const wrap = document.querySelector("#wrapper");
+const wmf = document.querySelector("#book-list li:nth-child(2) .name");
+//console.log(wmf);
 
-console.log(wrap);
+var books = document.querySelector("#book-list li .name");
+//console.log(books);
+
+var books = document.querySelectorAll("#book-list li .name");
+console.log(books);
+
+Array.from(books).forEach(function(book)
+{
+    console.log(book);
+});
